f1e9244502a69b9f8108269287abb9d2c67a844b
Shantanu Kumar <skumar@climaxstudios.com>
2026-03-10 14:00:06 +0000

    logger and its tests

---
+++ test/logger_test.cpp
@@ -0,0 +1,47 @@
+#include "gtest/gtest.h"
+#include "util/logger.h"
+#include <memory>
+
+class SpdlogTest : public ::testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        // Optional: setup logger for each test
+        // auto logger = spdlog::stdout_color_mt("test_logger");
+    }
+
+    void TearDown() override
+    {
+        spdlog::drop_all(); // safely remove all loggers after each test
+    }
+};
+
+namespace EETests
+{
+
+    TEST(Logger, StdLogger)
+    {
+        std::unique_ptr logger = std::make_unique<StdLogger>();
+        testing::internal::CaptureStdout();
+        logger->logf("Hello %d \n", 8);
+        std::string output = testing::internal::GetCapturedStdout();
+        EXPECT_EQ(output, "Hello 8 \n");
+    }
+
+    TEST_F(SpdlogTest, StdSpdLogger)
+    {
+        std::unique_ptr logger = std::make_unique<StdSpdLogger>();
+        logger->logf("Hello %d \n", 8);
+    }
+
+    TEST_F(SpdlogTest, RotatedFileLogger)
+    {
+        for (size_t i = 0; i <= 10; i++)
+        {
+            std::unique_ptr logger = std::make_unique<RotatedFileLogger>("./TestLogs/log.txt", 3);
+            logger->logf("Hello %d \n", i);
+        }
+    }
+
+}
