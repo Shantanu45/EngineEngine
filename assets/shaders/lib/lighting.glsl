05e570819ff1f25470af2c76194669e80a4264b9
Shantanu Kumar <skumar@climaxstudios.com>
2026-04-07 12:50:19 +0100

    light casters

--- assets/shaders/lib/lighting.glsl
+++ assets/shaders/lib/lighting.glsl
@@ -42,4 +42,70 @@ vec3 CalcPointLight(PointLight light, Material material, sampler2D diffuse_tex,
     return (ambient + diffuse + specular);
 }
 
+struct SpotLight {
+    vec3 position;
+    vec3 direction;
+    float cutOff;
+    float outerCutOff;
+  
+    float constant;
+    float linear;
+    float quadratic;
+  
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;       
+};
+
+vec3 CalcSpotLight(SpotLight light, Material material, sampler2D diffuse_tex,
+    sampler2D specular_tex, vec2 texCoords, vec3 normal, vec3 fragPos, vec3 viewDir)
+{
+    vec3 lightDir = normalize(light.position - fragPos);
+    // diffuse shading
+    float diff = max(dot(normal, lightDir), 0.0);
+    // specular shading
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    // attenuation
+    float distance = length(light.position - fragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + light.quadratic * (distance * distance));    
+    // spotlight intensity
+    float theta = dot(lightDir, normalize(-light.direction)); 
+    float epsilon = light.cutOff - light.outerCutOff;
+    float intensity = clamp((theta - light.outerCutOff) / epsilon, 0.0, 1.0);
+    // combine results
+    vec3 ambient = light.ambient * vec3(texture(diffuse_tex, texCoords));
+    vec3 diffuse = light.diffuse * diff * vec3(texture(diffuse_tex, texCoords));
+    vec3 specular = light.specular * spec * vec3(texture(specular_tex, texCoords));
+    ambient *= attenuation * intensity;
+    diffuse *= attenuation * intensity;
+    specular *= attenuation * intensity;
+    return (ambient + diffuse + specular);
+}
+
+struct DirLight {
+    vec3 direction;
+	
+    vec3 ambient;
+    vec3 diffuse;
+    vec3 specular;
+};
+
+vec3 CalcDirLight(DirLight light, Material material, sampler2D diffuse_tex,
+    sampler2D specular_tex, vec2 texCoords, vec3 normal, vec3 viewDir)
+{
+    vec3 lightDir = normalize(-light.direction);
+    // diffuse shading
+    float diff = max(dot(normal, lightDir), 0.0);
+    // specular shading
+    vec3 reflectDir = reflect(-lightDir, normal);
+    float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
+    // combine results
+    vec3 ambient = light.ambient * vec3(texture(diffuse_tex, texCoords));
+    vec3 diffuse = light.diffuse * diff * vec3(texture(diffuse_tex, texCoords));
+    vec3 specular = light.specular * spec * vec3(texture(specular_tex, texCoords));
+    return (ambient + diffuse + specular);
+}
+
+
 #endif
