ac0d4cc5ea63a9abcfb09d59268d26334512e4b2
Shantanu Kumar <skumar@climaxstudios.com>
2026-05-07 10:46:19 +0100

    ALLOC_MARK and FREE

--- src/util/profiler.h
+++ src/util/profiler.h
@@ -4,6 +4,8 @@
 #include "volk.h"
 #include <tracy/Tracy.hpp>
 #include <tracy/TracyVulkan.hpp>
+#define ALLOC_MARK(ptr, size, name) TracyAllocN(ptr, size, name)
+#define FREE_MARK(ptr, name)        TracyFreeN(ptr, name)
 #else
 // CPU zones
 #define FrameMark
@@ -14,6 +16,8 @@
 #define TracyFree(ptr)
 #define TracyAllocN(ptr, size, name)
 #define TracyFreeN(ptr, name)
+#define ALLOC_MARK(ptr, size, name)
+#define FREE_MARK(ptr, name)
 // Plots
 #define TracyPlot(name, val)
 // Vulkan GPU zones
